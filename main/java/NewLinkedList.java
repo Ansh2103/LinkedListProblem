e4945e845a84eb2e90ad9f306af3ce0211a5f083
Ansh2103 <swayamshubham007.sm@gmail.com>
2020-11-09 07:54:07 -0800

    Added : Functionality to test and add at last position

--- main/java/NewLinkedList.java
+++ main/java/NewLinkedList.java
@@ -8,17 +8,30 @@ public class NewLinkedList {
             this.last = null;
         }
 
-        public void addAtLast(Node myNode) {
-            if(last == null) {
-                last = myNode;
-            }
-            if(start == null){
-                start = myNode;
-            }
-            else {
-                Node temp = start;
-                this.start = myNode;
-                this.start.setNext(temp);
-            }
+    public void addAtStart(Node newFirstNode) {
+        if(last == null) {
+            last = newFirstNode;
         }
+        if(start == null){
+            start = newFirstNode;
+        }
+        else{
+            this.last.setNext(newFirstNode);
+            this.last = newFirstNode;
+        }
+    }
+
+    public void addAtLast(Node myNode) {
+        if(last == null) {
+            last = myNode;
+        }
+        if(start == null){
+            start = myNode;
+        }
+        else {
+            Node temp = start;
+            this.start = myNode;
+            this.start.setNext(temp);
+        }
+    }
     }
